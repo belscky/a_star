6edcd04d00a8a606eceb886c52038ea319654920
belscky <iaroslav.belscky@yandex.ru>
2021-09-26 18:31:48 +0300

    ITS WORKING YEAHHHH

---
+++ objects.h
@@ -0,0 +1,15 @@
+//
+// Created by mementomori on 26.09.2021.
+//
+#include <string>
+#ifndef A_STAR_OBJECTS_H
+#define A_STAR_OBJECTS_H
+using namespace std;
+struct Test {
+    string name;
+    Test(string _name="Oleg") {
+        name = _name;
+    }
+};
+
+#endif //A_STAR_OBJECTS_H
---
+++ test.cpp
@@ -0,0 +1,13 @@
+//
+// Created by mementomori on 26.09.2021.
+//
+#include <iostream>
+#include "objects.h"
+
+using namespace std;
+int main() {
+    Test one = Test("Alexy");
+    cout << one.name << endl;
+
+    return 0;
+}
